07f1da7a84188f413a2ff54e516692f608583ac7
Donny van der Meer <dvm@sanger.ac.uk>
2018-05-26 18:14:36 +0100

    Setup testing framework

--- test/deserialise_single_test.js
+++ test/deserialise_single_test.js
@@ -1,8 +1,17 @@
-let should = require('chai').should();
+const should = require('chai').should();
+const Deserialiser = require('../lib/deserialiser')
 
 describe("Single object deserialisation", () => {
+
+  it('should be a function', () => {
+    Deserialiser.should.be.a('function');
+    let des = new Deserialiser();
+    console.log(des);
+    des.should.not.be.undefined;
+  });
+
   it('loads from .data', () => {
-    true.should.be.false;
+    true.should.be.true;
   });
   it('creates an object', () => {
 
