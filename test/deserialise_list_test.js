af56840d27dc992275a6b52e544d1310b8847b7e
Donny van der Meer <dvm@sanger.ac.uk>
2018-05-26 17:29:42 +0100

    Setup project structure

---
+++ test/deserialise_list_test.js
@@ -0,0 +1,3 @@
+describe("list deserialisation", () => {
+
+})
---
+++ test/deserialise_single_test.js
@@ -0,0 +1,10 @@
+let should = require('chai').should();
+
+describe("Single object deserialisation", () => {
+  it('loads from .data', () => {
+    true.should.be.false;
+  });
+  it('creates an object', () => {
+
+  });
+});
